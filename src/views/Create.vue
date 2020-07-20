db99cf91e666c7371ae567e31e70b2d969a56967
Vyacheslav <vyacheslav.belozerov@rutorika.ru>
2020-07-20 22:56:25 +0300

    begin todo

---
+++ src/views/Create.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="row">
+    <div class="col s6 offset-s3">
+      <h1>Create</h1>
+      <form @submit.prevent="submitted">
+        <div class="input-field ">
+          <input id="title" v-model="title " type="text" class="validate" required>
+          <label for="title">Title</label>
+          <span class="helper-text" data-error="Title is required"></span>
+        </div>
+        <div class="chips" ref="chips"></div>
+        <div class="input-field">
+          <textarea id="description" class="materialize-textarea" v-model="description" maxlength="2048"></textarea>
+          <label for="description">Textarea</label>
+          <span class="character-counter" style="float: right; font-size: 12px;">{{description.length}}/2048</span>
+        </div>
+        <input type="text" ref="datepicker">
+        <button class="btn" type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Create',
+  data: () => ({
+    description: '',
+    title: '',
+    chips: null,
+    date: null
+  }),
+  computed: {
+  },
+  mounted() {
+    this.chips = M.Chips.init(this.$refs.chips, {
+      placeholder: 'Task tags'
+    })
+    this.date = M.Datepicker.init(this.$refs.datepicker,{
+      format: 'dd.mm.yyyy',
+      defaultDate: new Date,
+      setDefaultDate: true
+    });
+  },
+  methods: {
+    submitted() {
+      const task = {
+        title: this.title,
+        description: this.description,
+        id: Date.now(),
+        status: 'active',
+        tags: this.chips.chipsData,
+        date: this.date.date
+      }
+      console.log(task)
+    }
+  },
+  destroyed() {
+    if (this.date && this.date.destroy) {
+      this.date.destroy();
+    }
+    if (this.chips && this.chips.destroy) {
+      this.chips.destroy();
+    }
+  }
+}
+</script>
